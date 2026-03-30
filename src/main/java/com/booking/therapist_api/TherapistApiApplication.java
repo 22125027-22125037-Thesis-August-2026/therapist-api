c0ec9afcef050358ab05afb474cd47c697f00215
Dương Ngọc Quang Khiêm <128200532+KhimDuong@users.noreply.github.com>
2026-03-30 14:20:21 +0700

    Initial commit

---
+++ src/main/java/com/booking/therapist_api/TherapistApiApplication.java
@@ -0,0 +1,13 @@
+package com.booking.therapist_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TherapistApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TherapistApiApplication.class, args);
+	}
+
+}
