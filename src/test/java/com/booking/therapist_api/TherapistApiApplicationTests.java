3052751786c17fcac8c9ec83de92a35dc2470d84
Dương Ngọc Quang Khiêm <128200532+KhimDuong@users.noreply.github.com>
2026-04-09 15:22:59 +0700

    Phase 16: Reviews & Ratings Tested and Bugs fixed done

--- src/test/java/com/booking/therapist_api/TherapistApiApplicationTests.java
+++ src/test/java/com/booking/therapist_api/TherapistApiApplicationTests.java
@@ -2,8 +2,10 @@ package com.booking.therapist_api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TherapistApiApplicationTests {
 
 	@Test
